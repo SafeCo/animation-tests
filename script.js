473f22ca04d1dbd45190c4abd8987172c23bdef9
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-02-20 20:20:23 +0000

    Circle goes towards button
    Circle now goes towards button, need to make it so the animation ends when the circle make contact with the center as opposed to the top left.

--- script.js
+++ script.js
@@ -3,41 +3,41 @@ const pause= document.querySelector('.pause')
 const come = document.querySelector('.come')
 const remove = document.querySelector('.remove')
 const r = document.querySelector(':root');
-
+const removeT = document.querySelector('.remove-translate')
 
 
 pause.addEventListener("click", function(){
     circle.style.animationPlayState = 'paused'    
-    let position = circle.getBoundingClientRect()
-    console.log('when paused')
-    console.log(position)
+  
 })
 
 remove.addEventListener('click', function(){
+    
     let position = circle.getBoundingClientRect()
     r.style.setProperty('--circle-top', position.top+'px');
     r.style.setProperty('--circle-left', position.left+'px');
+    // circle.style.transform = 'translate(-50%,-50%)';
     circle.style.animation = 'none'
-    console.log('when animation removed')
-    console.log(position)
+   
+})
+
+removeT.addEventListener('click', function(){
+    circle.style.animation = 'none'
+    circle.style.transform = 'translate(-50%,-50%)'
 })
 
 
-
-
 come.addEventListener('click', function(){
-    let cposition = come.getBoundingClientRect()
+    let circlePosition = come.getBoundingClientRect()
     let position = circle.getBoundingClientRect()
-    console.log('when animation come intialised first is button, second is circle')
-    console.log(cposition)
-    console.log(position)
-    r.style.setProperty('--come-top', cposition.top+'px');
-    r.style.setProperty('--come-left', cposition.left+'px');
-    circle.style.animation = 'come 3s linear infinite'
-    console.log('when animation come executed firs is button, second is circle')
-    console.log(cposition)
-    console.log(position)
+    
+    let newLeftPosition = circlePosition.top - position.top
+    let newTopPosition = circlePosition.left - position.left
 
+    r.style.setProperty('--come-top', newTopPosition+'px');
+    r.style.setProperty('--come-left', newLeftPosition+'px');
+    circle.style.animation = 'come 3s linear infinite'
+    
 })
 
 
