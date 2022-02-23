ff308907ee88b67ef54aa6f99a44ef1010fe97a0
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-02-23 22:45:13 +0000

    Locating co-ordinates relative to path
    managed to make animations in relation to path, however cannot get the co-ordinates of specific positions in the path.

--- GSAP animations/script.js
+++ GSAP animations/script.js
@@ -1,53 +1,85 @@
+const orbContainer = document.querySelector('.orb-container')
 const orbOne = document.querySelector('.one')
-const path = document.querySelector('#path')
-const origin = document.querySelector('.orb-position')
 const orbTwo = document.querySelector('.two')
+const path = document.querySelector('#path')
+const pathView = document.querySelector('#path-view')
+
+// let p =MotionPathPlugin.getRelativePosition(orbOne, pathView, [0.5, 0.5],{x:110,y:5})
+
+const tl = gsap.timeline({repeat:1});
+
+let rawPath = MotionPathPlugin.getRawPath("#path"),
+    pl;
+MotionPathPlugin.cacheRawPathMeasurements(rawPath);
+ 
 
 
 
+// for(i = 0; i < orbContainer.children.length; i++){
+
+//   const orbPathPositions = [[0, 1],[0.2, 1.2],[0.4, 1.4],[0.6, 1.6],[0.8, 1.8],[1, 2]]
+//   let orb = orbContainer.children[i]
+//   // let p =MotionPathPlugin.getRelativePosition(orb, pathView, [0.5, 0.5],{x:110,y:5})
+//   p = MotionPathPlugin.getPositionOnPath(rawPath, 0)
+  
+  
+//   tl.to(orb, {
+//     x: "+=" + p.x, 
+//     y: "+=" + p.y,
+//     duration: 4,
+//     ease: "power1.inOut"
+//   }, 0);
 
 
-let matrix = MotionPathPlugin.getAlignMatrix(origin, path, [0.5, 0.5], [0.5, 0.5]),
-    dragmePoint = {x: 0, y:0}, 
-    dotPoint = matrix.apply(dragmePoint);
 
-const tl = gsap.timeline({repeat:-1});
+//   tl.to(orb, {
+//       duration: 5, 
+//       repeat: -1,
+//       ease: "linear",
+//       motionPath:{
+//         start: orbPathPositions[i][0],
+//         end: orbPathPositions[i][1],
+//         path: "#path",
+//         align: "#path",
+//         alignOrigin: [0.5, 0.5]
+//       }
+//     });
+// }
+
+
+// let p =MotionPathPlugin.getRelativePosition(orbOne, pathView, [0.5, 0.5],{x:110,y:5})
+p = MotionPathPlugin.getPositionOnPath(rawPath, 1)
+
+let zl =MotionPathPlugin.getRelativePosition(orbOne, path, [0.5, 0.5],{x:p.x,y:p.y})
+let matrix = MotionPathPlugin.getGlobalMatrix(path),
+    // 0,0 is the top left corner, but you can use any local coordinates.
+    localPoint = {x: p.x, y: p.y}, 
+    // get a new point with the matrix transformations applied.
+    z = matrix.apply(localPoint);
+
+console.log(rawPath)
+console.log(p)
+console.log(z)
+console.log(zl)
 
 tl.to(orbOne, {
-  x: "+=" + dotPoint.x, 
-  y: "+=" + dotPoint.y,
+  x: "+=" + zl.x, 
+  y: "+=" + zl.y,
   duration: 4,
   ease: "power1.inOut"
-});
+}, 0);
+
+
 
 tl.to(orbOne, {
     duration: 5, 
-    repeat: -1,
+    repeat: 1,
     ease: "linear",
     motionPath:{
+      start: 0,
+      end: 1,
       path: "#path",
       align: "#path",
       alignOrigin: [0.5, 0.5]
     }
   });
-
-
-  const tlTwo = gsap.timeline({repeat:-1, delay: 1});
-
-  tlTwo.to(orbTwo, {
-    x: "+=" + dotPoint.x, 
-    y: "+=" + dotPoint.y,
-    duration: 4,
-    ease: "power1.inOut"
-  });
-  
-  tlTwo.to(orbTwo, {
-      duration: 5, 
-      repeat: -1,
-      ease: "linear",
-      motionPath:{
-        path: "#path",
-        align: "#path",
-        alignOrigin: [0.5, 0.5]
-      }
-    });
